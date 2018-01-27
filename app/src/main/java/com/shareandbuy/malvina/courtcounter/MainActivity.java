fcd43ee1c852235b3cf6bc8c6fb9f51c81010d25
kot2d <kot2d@outlook.com>
2018-01-27 10:49:16 +0300

    add luke combo

--- app/src/main/java/com/shareandbuy/malvina/courtcounter/MainActivity.java
+++ app/src/main/java/com/shareandbuy/malvina/courtcounter/MainActivity.java
@@ -3,7 +3,6 @@ package com.shareandbuy.malvina.courtcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -18,8 +17,12 @@ public class MainActivity extends AppCompatActivity {
     int darthVaderAttack = 0;
     TextView empireScore;
     TextView rebelScore;
+    TextView deathStarScore;
+    TextView yavinScore;
     LinearLayout rebelFullLayout;
     LinearLayout empireFullLayout;
+    int luke_combo = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         this.milFalconAttact = Integer.parseInt(getString(R.string.millennium_falcon_damage));
         this.tieFighterAttack = Integer.parseInt(getString(R.string.tie_fighter_damage));
         this.darthVaderAttack = Integer.parseInt(getString(R.string.darth_vader_tie_fighter_damage));
+        this.deathStarScore = findViewById(R.id.death_star_score);
+        this.yavinScore = findViewById(R.id.yavin_score);
 
         this.empireScore = findViewById(R.id.empire_total_ships);
         this.empireFullLayout = (LinearLayout) this.empireScore.getParent();
@@ -40,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         this.empireScore.setText(String.valueOf(this.empire_ships_amount));
         this.rebelScore.setText(String.valueOf(this.rebel_ships_amount));
+        this.deathStarScore.setText(getString(R.string.death_star_live_status));
+        this.yavinScore.setText(getString(R.string.yavin_live_status));
     }
 
     public void attack(View view){
@@ -53,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 this.empire_ships_amount = this.empire_ships_amount - this.lukeAttack;
                 this.empireScore.setText(String.valueOf(this.empire_ships_amount));
                 setAttackedSection(this.empireScore.getId());
+                this.luke_combo += 1;
                 break;
             case R.id.mel_falcon_attack:
                 this.empire_ships_amount = this.empire_ships_amount - this.milFalconAttact;
@@ -70,21 +78,39 @@ public class MainActivity extends AppCompatActivity {
                 setAttackedSection(this.rebelScore.getId());
                 break;
         }
+
+        if(this.luke_combo == 3){
+            this.deathStarScore.setText(getString(R.string.death_star_destroyed_status));
+        }
+
     }
 
     void setAttackedSection(int layoutId){
-        if(layoutId == rebelScore.getId()){
-            rebelScore.setTextColor(getResources().getColor(R.color.attacked_text));
-            rebelFullLayout.setBackgroundColor(getResources().getColor(R.color.attacked_layout));
-            empireScore.setTextColor(getResources().getColor(R.color.title_text));
-            empireFullLayout.setBackgroundColor(getResources().getColor(R.color.score_container));
+        if(layoutId == this.rebelScore.getId()){
+            this.deathStarScore.setTextColor(getResources().getColor(R.color.attacked_text));
+            this.rebelFullLayout.setBackgroundColor(getResources().getColor(R.color.attacked_layout));
+            this.empireScore.setTextColor(getResources().getColor(R.color.black));
+            this.empireFullLayout.setBackgroundColor(getResources().getColor(R.color.yellow));
             return;
         }
-        if(layoutId == empireScore.getId()){
-            empireScore.setTextColor(getResources().getColor(R.color.attacked_text));
-            empireFullLayout.setBackgroundColor(getResources().getColor(R.color.attacked_layout));
-            rebelScore.setTextColor(getResources().getColor(R.color.title_text));
-            rebelFullLayout.setBackgroundColor(getResources().getColor(R.color.score_container));
+        if(layoutId == this.empireScore.getId()){
+            this.empireScore.setTextColor(getResources().getColor(R.color.attacked_text));
+            this.empireFullLayout.setBackgroundColor(getResources().getColor(R.color.attacked_layout));
+            this.rebelScore.setTextColor(getResources().getColor(R.color.black));
+            this.rebelFullLayout.setBackgroundColor(getResources().getColor(R.color.yellow));
         }
+    }
+
+    public void reset(View view){
+        this.rebelScore.setTextColor(getResources().getColor(R.color.black));
+        this.rebelFullLayout.setBackgroundColor(getResources().getColor(R.color.yellow));
+        this.empireScore.setTextColor(getResources().getColor(R.color.black));
+        this.empireFullLayout.setBackgroundColor(getResources().getColor(R.color.yellow));
+        this.empire_ships_amount = Integer.parseInt(getString(R.string.empire_total_ships));
+        this.rebel_ships_amount = Integer.parseInt(getString(R.string.rebel_total_ships));
+        this.empireScore.setText(String.valueOf(this.empire_ships_amount));
+        this.rebelScore.setText(String.valueOf(this.rebel_ships_amount));
+        this.deathStarScore.setText(getString(R.string.death_star_live_status));
+        this.yavinScore.setText(getString(R.string.yavin_live_status));
     }
 }
